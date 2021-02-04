5916ce55f315d283d31560f919bebd76157ac730
Deepak D <deepakcdo@gmail.com>
2021-02-04 22:02:46 +0000

    Fix amps to run

--- amps/amps-server/manifest/configmap/run.sh
+++ amps/amps-server/manifest/configmap/run.sh
@@ -3,8 +3,12 @@ SET_INDEX=${HOSTNAME##*-}
 echo "Starting initializing for pod $SET_INDEX"
 if [ "$SET_INDEX" = "0" ]; then
   cp /mnt/scripts/LDN-Primary.xml /mnt/data/amps-config.xml
+  echo LDN-Primary > /mnt/data/AMPS_NAME
+  echo amps-1 > /mnt/data/REP_SERVER
 elif [ "$SET_INDEX" = "1" ]; then
   cp /mnt/scripts/LDN-Backup.xml /mnt/data/amps-config.xml
+    echo LDN-Backup > /mnt/data/AMPS_NAME
+    echo amps-0 > /mnt/data/REP_SERVER
 else
   echo "Invalid statefulset index"
   exit 1
